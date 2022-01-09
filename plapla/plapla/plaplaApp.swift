1efa7adcac15c28231dd8a43fa930a2aaae71ad4
kenta-maeda-rv <88298400+kenta-maeda-rv@users.noreply.github.com>
2022-01-09 14:43:08 +0900

    Add files via upload

---
+++ plapla/plapla/plaplaApp.swift
@@ -0,0 +1,17 @@
+//
+//  plaplaApp.swift
+//  plapla
+//
+//  Created by 前田 健太 (Maeda Kenta) on 2022/01/09.
+//
+
+import SwiftUI
+
+@main
+struct plaplaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
