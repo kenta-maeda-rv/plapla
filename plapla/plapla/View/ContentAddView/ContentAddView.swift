e8866f3068fce1f85d5daa4f38fe8e213b1e85aa
前田 健太 (Maeda Kenta) <ke-maeda@cec-ltd.co.jp>
2022-03-14 20:53:40 +0900

    コンテンツ追加画面修正

--- plapla/plapla/View/ContentAddView/ContentAddView.swift
+++ plapla/plapla/View/ContentAddView/ContentAddView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct ContentAddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    @StateObject var contentAddViewModel: ContentAddViewModel = ContentAddViewModel()
+    
+    @State var titleText = ""
+    
     var body: some View {
-        Text("ContentAddView")
+        ScrollView(.vertical) {
+            
+            Button(action: {
+                
+            }) {
+                Text("ImageAdd")
+            }
+            .frame(width: 300,
+                   height: 300,
+                   alignment: .center)
+            .background(Color.gray)
+            
+            TextField("contentTitle", text: $titleText)
+                .padding(30)
+                .background(Color.gray)
+            
+            Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("追加")
+            }
+            .padding(150)
+            .frame(height: 80)
+            .background(Color.gray)
+        }
     }
 }
 
--- plapla/plapla/View/ContentAddView/ContentAddViewModel.swift
+++ plapla/plapla/View/ContentAddView/ContentAddViewModel.swift
@@ -5,4 +5,8 @@
 //  Created by 前田 健太 (Maeda Kenta) on 2022/03/14.
 //
 
-import Foundation
+import SwiftUI
+
+class ContentAddViewModel: ObservableObject {
+    
+}
